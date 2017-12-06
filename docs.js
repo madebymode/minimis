1fbac196b2004b074d4c2414db96c724d36dec2d
Tanner Hodges <austintannerhodges@gmail.com>
2017-12-06 14:41:50 -0500

    Remove old highlight.js language comments

--- docs.js
+++ docs.js
@@ -1,11 +1,6 @@
 // Code Highlighting
 import hljs from 'highlight.js/lib/highlight';
-// hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
 hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
-// hljs.registerLanguage('css', require('highlight.js/lib/languages/css'));
-// hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'));
 hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
-// hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
-// hljs.registerLanguage('php', require('highlight.js/lib/languages/php'));
 hljs.registerLanguage('scss', require('highlight.js/lib/languages/scss'));
 hljs.initHighlightingOnLoad();
