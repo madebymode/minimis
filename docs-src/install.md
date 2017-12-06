302988bc6eec8e3ed6045fd3f88163f1f7dd68b4
Tanner Hodges <austintannerhodges@gmail.com>
2017-12-06 14:04:27 -0500

    Syntax highlighting

--- docs-src/install.md
+++ docs-src/install.md
@@ -6,26 +6,26 @@ hologram: true
 ---
 ## 1. Install with [npm](https://docs.npmjs.com/getting-started/what-is-npm)
 
-```
+```js_example
 npm i minimis --save-dev
 ```
 
 ## 2. Copy starter kit
 
-```
+```js_example
 mkdir -p resources/assets/sass/
 rsync -avz node_modules/minimis/starter-kit/ resources/assets/sass/
 ```
 
 ## 3. Build with [gulp](https://gulpjs.com/)
 
-```
+```js_example
 npm i gulp gulp-sass npm-sass gulp-sourcemaps gulp-autoprefixer gulp-clean-css --save-dev
 ```
 
 ### Sample `gulpfile.js`
 
-```js
+```js_example
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sassImporter = { importer: require('npm-sass').importer };
@@ -59,7 +59,7 @@ gulp.task('watch', function() {
 
 ## 4. <small class="optional">Optional</small> Keep clean code with [EditorConfig](http://editorconfig.org/) and [stylelint](http://stylelint.io/)
 
-```
+```js_example
 npm i stylelint-config-property-sort-order-smacss --save-dev
 cp node_modules/minimis/.editorconfig .editorconfig
 cp node_modules/minimis/.stylelintrc .stylelintrc
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,55 @@
 const gulp = require('gulp');
+
+gulp.task('default', ['scripts', 'styles']);
+
+gulp.task('watch', function() {
+  gulp.watch(SASS_SRC, ['styles']);
+  // TODO: JS watch task?
+  // gulp.watch(JS_PATHS, ['js']);
+});
+
+
+
+// ------------------------------
+// JS
+// ------------------------------
+
+const webpack = require('webpack-stream');
+const named = require('vinyl-named');
+
+const JS_CONFIG = require('./webpack.config.js');
+const JS_PATHS = ['./docs.js'];
+const JS_DEST = './docs/js';
+
+gulp.task('scripts', function() {
+  return gulp.src(JS_PATHS)
+    .pipe(named())
+    .pipe(webpack(JS_CONFIG))
+    .pipe(gulp.dest(JS_DEST));
+});
+
+
+
+// ------------------------------
+// Sass
+// ------------------------------
+
 const sass = require('gulp-sass');
+const sassImporter = { importer: require('npm-sass').importer };
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
-// Sass
-const SASS_PATHS = ['./docs.scss'];
+const SASS_SRC = ['./docs.scss'];
 const SASS_DEST = './docs/css';
 
-gulp.task('sass', function() {
-  return gulp.src(SASS_PATHS)
-    .pipe(sass())
+gulp.task('styles', function() {
+  return gulp.src(SASS_SRC)
+    .pipe(sass(sassImporter))
     .pipe(sourcemaps.init())
     .pipe(autoprefixer())
     .on('error', sass.logError)
     .pipe(cleanCSS({ inline: ['none'], keepSpecialComments: 0 }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(SASS_DEST));
-});
-
-// Default
-gulp.task('default', ['sass']);
-
-// Watch
-gulp.task('watch', function() {
-  gulp.watch(SASS_PATHS, ['sass']);
 });
